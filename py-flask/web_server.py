044bc9bab6de5f468115a3dec1d6bb78e2097a4c
Sean <opjlmi@gmail.com>
2015-01-15 15:11:45 +0800

    check app setting

--- py-flask/web_server.py
+++ py-flask/web_server.py
@@ -279,6 +279,6 @@ if __name__ == '__main__':
     app.logger.addHandler(handler)
     ###### for LOG ######
 
-#   app.run(host='0.0.0.0',port=80)
+    # app.run(host='0.0.0.0',port=80,threaded=True)
     app.run(threaded=True)
 
