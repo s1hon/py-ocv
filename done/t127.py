436e74fd728ee45bc78fb45e3a76bd58366f3ce1
joy8303 <4A139049@gmail.com>
2015-02-04 09:49:31 +0800

    ocv update dir3

--- done/t127.py
+++ done/t127.py
@@ -13,7 +13,7 @@ def direction0(q,gimg):
         	for y in range(width):
             		if gimg[x][y]<=223: # black
                 		if (gimg[x][y-1]>223 or y==0): # white || y=0
-                    			q_tmp += "G0 X" + str(x/20) + " Y" + str(y/20) + "\n"
+                   			q_tmp += "G0 X" + str(x/20) + " Y" + str(y/20) + "\n"
                 		elif y==(width-1): # if y has gone end.
                     			q_tmp += "G1 X" + str(x/20) + " Y" + str(y/20) + "\n"
             		elif (gimg[x][y-1]<=223 and y>0): # black && y > 0
@@ -38,29 +38,37 @@ def direction1(q,gimg):
     	q.close
     	print("d1 DONE!")
 
+
 def direction2(q,gimg):
+	height, width = gimg.shape
 	q_tmp=''
-	width=3
-	height=5
+	intr=21.0
 	the_range=[]
-	for x in range(width-1,0,-1):
+	for x in range(height-1,0,-21):
 		the_range.append([x,0])
-	for y in range(0,height,1):
+	for y in range(0,width,21):
 		the_range.append([0,y])
 	print(the_range)		
 	for t_range in the_range:
 		print t_range	
 		tx=t_range[0]
 		ty=t_range[1]
-		q_tmp += "G0 X" + str(tx) + " Y" + str(ty) + "\n"
+		q_tmp += "G0 X" + str(tx/intr) + " Y" + str(ty/intr) + "\n"
 		print "X %d Y %d" %(tx,ty)
-		while (tx>=0 and tx<=width and ty>=0 and ty<=height ):
+		while (tx>=0 and tx+intr<height and ty>=0 and ty+intr<width ):
 			print "X1 %d Y1 %d" %(tx,ty)
-			q_tmp += "G1 X" + str(tx) + " Y" + str(ty) + "\n"	
-			tx+=1
-			ty+=1
-
-
+            		if gimg[tx][ty]<=159: # black
+                		if (tx+intr<=height and ty+intr<=width) :
+					if (gimg[tx+intr][ty+intr]<=159 or ty==0): # white || y=0
+                    				q_tmp += "G1 X" + str(tx/intr) + " Y" + str(ty/intr) + "\n"
+                			elif ty==width: # if y has gone end.
+                    				q_tmp += "G0 X" + str(tx/intr) + " Y" + str(ty/intr) + "\n"					
+            		elif (tx+intr<height and ty+intr<width) :
+				if (gimg[tx+intr][ty+intr]<=159 and ty>0): # black && y > 0
+                			q_tmp += "G0 X" + str(tx/intr) + " Y" + str((ty-1)/intr) + "\n"					
+			tx+=intr
+			ty+=intr
+		
 #	height, width = gimg.shape
 #        q_tmp=''
 	
@@ -95,25 +103,25 @@ if __name__ == '__main__':
     	q0x,q0 = Pipe()
     	q1x,q1 = Pipe()
 	q2x,q2 = Pipe()
-    	p0 = Process(target=direction0,args=(q0,gimg,))
+   	p0 = Process(target=direction0,args=(q0,gimg,))
     	p1 = Process(target=direction1,args=(q1,gimg,))
 	p2 = Process(target=direction2,args=(q2,gimg,))
-#    	p0.start()
-#    	p1.start()
+    	p0.start()
+    	p1.start()
 	p2.start()
 
 
-#    	q0_r = q0x.recv()
-#    	q1_r = q1x.recv()
+    	q0_r = q0x.recv()
+    	q1_r = q1x.recv()
 	q2_r = q2x.recv()
     	print("End of Get the Pipe....")
 
 
-#    	p0.join()
-#    	print("p0 end!")
+    	p0.join()
+    	print("p0 end!")
 
-#    	p1.join()
-#    	print("p1 end!")
+    	p1.join()
+    	print("p1 end!")
 	
 	p2.join()
        	print("p1 end!")
@@ -123,8 +131,7 @@ if __name__ == '__main__':
     	filename='t127'
     	file_id = str(filename) + '.nc'
     	f = open(file_id,'w')
-#    	f.write(q0_r+q1_r)
-	f.write(q2_r)
+    	f.write(q0_r+q1_r+q2_r)
     	f.close()
 
 
