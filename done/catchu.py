42e3b1260a1ab03c29c87aefc8977944758a9d2e
Sean <opjlmi@gmail.com>
2015-02-08 20:18:04 +0800

    Add auto creating mode & auto creating pictur

--- done/catchu.py
+++ done/catchu.py
@@ -4,106 +4,152 @@ import os
 import time
 from multiprocessing import Process, Pipe
 
-#make file save g-code
-# def makefile(filename):
-#   file_id = str(filename) + '.nc'
-#   f = open(file_id,'w')
-#   f.close()
-#   return file_id
-
+from progressbar import AnimatedMarker, Bar, BouncingBar, Counter, ETA, \
+    FileTransferSpeed, FormatLabel, Percentage, \
+    ProgressBar, ReverseBar, RotatingMarker, \
+    SimpleProgress, Timer, AdaptiveETA, AdaptiveTransferSpeed
 
 #direction top to bottom
-def direction1(q,gimg):
+def direction0(q,gimg,level):
+    color = GetLevel(level)
     height, width = gimg.shape
+    intr=5
+    zoom=20.0
     q_tmp="G17\nM3 S1000\nG0 X0 Y0\n"
-    for x in range(0,height,10):
+    for x in range(0,height,intr):
         for y in range(width):
-            if gimg[x][y]<=223: # black
-                if (gimg[x][y-1]>223 or y==0): # white || y=0
-                    q_tmp += "G0 X" + str(x/20) + " Y" + str(y/20) + "\n"
+            if gimg[x][y]<=color: # black
+                if (gimg[x][y-1]>color or y==0): # white || y=0
+                    q_tmp += "G0 X" + str(x/zoom) + " Y" + str(y/zoom) + "\n"
                 elif y==(width-1): # if y has gone end.
-                    q_tmp += "G1 X" + str(x/20) + " Y" + str(y/20) + "\n"
-            elif (gimg[x][y-1]<=223 and y>0): # black && y > 0
-                q_tmp += "G1 X" + str(x/20) + " Y" + str((y-1)/20) + "\n"
+                    q_tmp += "G1 X" + str(x/zoom) + " Y" + str(y/zoom) + "\n"
+            elif (gimg[x][y-1]<=color and y>0): # black && y > 0
+                q_tmp += "G1 X" + str(x/zoom) + " Y" + str((y-1)/zoom) + "\n"
     q.send(q_tmp)
     q.close
-    print("d1 DONE!")
 
-def direction2(q,gimg):
+def direction1(q,gimg,level):
+    color = GetLevel(level)
     height, width = gimg.shape
     q_tmp=''
-    for y in range(0,width,10):
+    intr=5
+    zoom=20.0
+    for y in range(0,width,intr):
         for x in range(height):
-		if gimg[x][y]<=191: # black
-                	if (gimg[x-1][y]>191 or x==0): # white || y=0
-                    		q_tmp += "G0 X" + str(x/20) + " Y" + str(y/20) + "\n"
-                	elif x==(height-1): # if y has gone end.
-                    		q_tmp += "G1 X" + str(x/20) + " Y" + str(y/20) + "\n"
-            	elif (gimg[x-1][y]<=191 and x>0): # black && y > 0
-                	q_tmp += "G1 X" + str((x-1)/20) + " Y" + str(y/20) + "\n"
+            if gimg[x][y]<=color: # black
+                if (gimg[x-1][y]>color or x==0): # white || y=0
+                    q_tmp += "G0 X" + str(x/zoom) + " Y" + str(y/zoom) + "\n"
+                elif x==(height-1): # if y has gone end.
+                    q_tmp += "G1 X" + str(x/zoom) + " Y" + str(y/zoom) + "\n"
+            elif (gimg[x-1][y]<=color and x>0): # black && y > 0
+                q_tmp += "G1 X" + str((x-1)/zoom) + " Y" + str(y/zoom) + "\n"
     q.send(q_tmp)
     q.close
-    print("d2 DONE!")
 
 
+def direction2(q,gimg,level):
+    color = GetLevel(level)
+    height, width = gimg.shape
+    q_tmp=''
+    intr=5
+    zoom=20.0
+    the_range=[]
+
+    for x in range(height-1,0,-intr):
+        the_range.append([x,0])
+    for y in range(0,width,intr):
+        the_range.append([0,y])
+    for t_range in the_range:
+        tx=t_range[0]
+        ty=t_range[1]
+        q_tmp += "G0 X" + str(tx/zoom) + " Y" + str(ty/zoom) + "\n"
+        while (tx>=0 and tx+intr<height and ty>=0 and ty+intr<width ):
+            if gimg[tx][ty]<=color: # black
+                if (tx+intr<=height and ty+intr<=width) :
+                    if (gimg[tx+intr][ty+intr]<=color or ty==0): # white || y=0
+                        q_tmp += "G1 X" + str(tx/zoom) + " Y" + str(ty/zoom) + "\n"
+                    elif ty==width: # if y has gone end.
+                        q_tmp += "G0 X" + str(tx/zoom) + " Y" + str(ty/zoom) + "\n"
+            elif (tx+intr<height and ty+intr<width) :
+                if (gimg[tx+intr][ty+intr]<=color and ty>0): # black && y > 0
+                    q_tmp += "G0 X" + str(tx/zoom) + " Y" + str((ty-1)/zoom) + "\n"
+            tx+=intr
+            ty+=intr
+
+    q.send(q_tmp)
+    q.close
+
+
+def direction3(q,gimg,level):
+    pass
+
+
+def GetLevel(level):
+    if level == 1 :
+        return 223
+    elif level == 2 :
+        return 191
+    elif level == 3 :
+        return 159
+    elif level == 4 :
+        return 127
+    elif level == 5 :
+        return 95
+    elif level == 6 :
+        return 63
+    elif level == 7 :
+        return 31
 
 
 def Gcode_Creater(print_id):
-    #pic to gray
-    gimg = cv2.imread( print_id +'.jpg',cv2.IMREAD_GRAYSCALE)
+    # bar status
+    pbar = ProgressBar(widgets=[Percentage(), Bar()], maxval=100).start()
 
+    #pic to gray
+    gimg = cv2.imread('./static/upload_pic/'+print_id +'.jpg',cv2.IMREAD_GRAYSCALE)
     # parent_conn, child_conn = Pipe()
     q0x,q0 = Pipe()
     q1x,q1 = Pipe()
-
-    p0 = Process(target=direction1,args=(q0,gimg,))
-    p1 = Process(target=direction2,args=(q1,gimg,))
+    q2x,q2 = Pipe()
+    # q3x,q3 = Pipe()
+    p0 = Process(target=direction0,args=(q0,gimg,1,))
+    p1 = Process(target=direction1,args=(q1,gimg,3,))
+    p2 = Process(target=direction2,args=(q2,gimg,5,))
+    # p3 = Process(target=direction3,args=(q3,gimg,7,))
     p0.start()
     p1.start()
+    p2.start()
+    # p3.start()
+
+    pbar += 10
 
     q0_r = q0x.recv()
     q1_r = q1x.recv()
-    print("End of Get the Pipe....")
+    q2_r = q2x.recv()
+    # q3_r = q3x.recv()
 
     p0.join()
-    print("p0 end!")
+    pbar += 20
 
     p1.join()
-    print("p1 end!")
+    pbar += 20
 
-    print("Make the file.....")
-    file_id = str(print_id) + '.nc'
-    f = open(file_id,'w')
-    f.write(q0_r+q1_r)
+    p2.join()
+    pbar += 20
+
+    # p3.join()
+    pbar += 20
+
+
+    f = open('./static/gcodes/'+str(print_id) + '.nc','w')
+    f.write(q0_r)
     f.write(q1_r)
+    f.write(q2_r)
+    # f.write(q3_r)
     f.close()
-    print("Done!")
-
-
-if __name__ == '__main__':
-    Gcode_Creater('001049')
-
-#os.system(cat t127.nc  >>  t127-2.nc)
-
-
-#if child0!=0:
-#   child1=os.fork()
-
-#if child0==0:
-#   ppid0=os.getpid()
-#   print "Child0 PID:%d" %(ppid0)
-#   makefile(ppid0)
-#   direction1()
-#elif child1==0:
-#   ppid1=os.getpid()
-#   print "Child1 PID:%d" %(ppid1)
-#       makefile(ppid1)
-#        direction2()
-#else:
-#   print "Parent PID:%d" %(os.getpid())
-
-
-
+    pbar += 10
+    time.sleep(0.5)
+    pbar.finish()
 
 #cv2.imshow('pic-gray',gimg)
 #cv2.waitKey(0)
--- py-flask/web_server.py
+++ py-flask/web_server.py
@@ -4,6 +4,8 @@ if sys.getdefaultencoding() != 'utf-8':
     reload(sys)
     sys.setdefaultencoding('utf-8')
 
+import cv2
+import numpy as np,sys
 
 import json,requests
 # all the imports
@@ -13,11 +15,17 @@ from flask import Flask, request, session, g, redirect, url_for, \
 from contextlib import closing
 from logging.handlers import RotatingFileHandler
 from werkzeug.contrib.fixers import ProxyFix
+from werkzeug import secure_filename
 
+import logging
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
 
 # for creating gcode
 from multiprocessing import Process
 import time
+
+import catchu
 
 # configuration
 DATABASE = './pt_tab.db'
@@ -27,8 +35,12 @@ USERNAME = 'admin'
 PASSWORD = '123456'
 SU_CON = None
 HOSTWEB = 'http://120.117.73.74'
+UPLOAD_FOLDER = './static/upload_pic/'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config.from_object(__name__)
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
@@ -86,29 +98,64 @@ def show_entries(grbl=0):
 def add_entry():
     error=None
     if request.method == 'POST':
-        if request.form['stu_id'] != "" or request.form['name'] != "" or request.form['phone'] != "" :
+        # Get form text
+        stu_id = request.form['stu_id'].strip()
+        name = request.form['name'].strip()
+        phone = request.form['phone'].strip()
+        user_file = request.files['file']
+
+        if not stu_id or not name or not phone or not user_file or not allowed_file(user_file.filename):
+            error=''
+            if not stu_id:
+                error+='學號 錯誤或未填寫。<br>'
+            if not name:
+                error+='姓名 錯誤或未填寫。<br>'
+            if not phone:
+                error+='手機 錯誤或未填寫。<br>'
+            if not user_file or not allowed_file(user_file.filename):
+                error+='檔案 錯誤。(限png,jpg,jpeg,gif)'
+
+        else:
+
             totalmax = g.db.execute("select count(*) from prints")
             totalmax = totalmax.fetchone()
             totalmax = "%03d" %(totalmax[0]+1)
+            print_id = totalmax + request.form['stu_id'][-3:]
 
+            # Uploading Picture
+            if user_file and allowed_file(user_file.filename):
+                filename = secure_filename(user_file.filename)
+                user_file.save(os.path.join(app.config['UPLOAD_FOLDER'], print_id+'_tmp.'+user_file.filename.rsplit('.', 1)[1]))
+                # Resize image
+                image_resize( print_id+'_tmp.'+user_file.filename.rsplit('.', 1)[1] )
+                os.system('rm -rf '+app.config['UPLOAD_FOLDER']+print_id+'_tmp.'+user_file.filename.rsplit('.', 1)[1])
+
+            # Creating Data
             g.db.execute('insert into prints (print_id, stu_id, name, phone, status) values (?,?,?,?,?)',
-                         [totalmax + request.form['stu_id'][-3:], request.form['stu_id'], request.form['name'],request.form['phone'],0])
+                         [print_id, request.form['stu_id'], request.form['name'],request.form['phone'],0])
             g.db.commit()
 
             # Information for html
-            flash('請記下您的列印件號： '+totalmax + request.form['stu_id'][-3:],'alert-success btn-lg flash-bg')
-            app.logger.info('[Print] Add Printing <PID:'+totalmax + request.form['stu_id'][-3:]+'>')
+            flash('請記下您的列印件號： '+print_id,'alert-success btn-lg flash-bg')
+            app.logger.info('[Print] Add Printing <PID:'+print_id+'>')
 
             # Background creating gcode
-            print_id=totalmax + request.form['stu_id'][-3:]
-            gcode = Process(target=gcode_creater, args=(print_id,))
+            gcode = Process(target=gcode_done, args=(print_id,))
             gcode.start()
 
             return redirect(url_for('show_entries'))
-        else:
-            error='您輸入的資料有誤。'
+
     title="新增列印"
     return render_template('add.html',title=title,error=error,printnow=get_print_now_id())
+
+
+@app.route('/edit', methods=['GET', 'POST'])
+def resize():
+    if request.method == 'POST':
+        pass
+    return render_template('resize.html')
+
+
 
 # 搜尋/修改單
 @app.route('/pid', methods=['GET', 'POST'])
@@ -158,19 +205,20 @@ def edit_pid_entry(print_id):
 def manage_entry():
     if request.method == 'POST':
 
-        # for hack
         search_pid=g.db.execute('select print_id from prints where print_id="'+request.form['print_id']+'"')
         search_pid=search_pid.fetchone()
-        if not search_pid :
-            abort(401)
+
+        if search_pid == None :
+            flash('執行動作失敗，請聯絡管理者。','alert-danger')
+            return redirect(url_for('show_entries'))
 
         # which button press down
         if request.form['submit'] == "修改":
             return redirect(url_for('edit_pid_entry',print_id=str(request.form['print_id'])))
         elif request.form['submit'] == "刪除":
-            # delete from prints where print_id="002011";
             g.db.execute('delete from prints where print_id="'+search_pid[0]+'"')
             g.db.commit()
+            os.system('rm -rf '+app.config['UPLOAD_FOLDER']+search_pid[0]+'.jpg')
             flash('刪除成功。','alert-danger')
 
             app.logger.error('[Print] Delete Printing <PID:'+search_pid[0]+'>')
@@ -195,12 +243,30 @@ def manage_entry():
             else:
                 flash('尚有列印工作進行中。','alert-danger')
                 return redirect(url_for('show_entries'))
+
         elif request.form['submit'] == "停止":
+
+            stop_pid=g.db.execute('select print_id from prints where status="3" and print_id="'+request.form['print_id']+'"')
+            stop_pid=stop_pid.fetchone()
+
+            if stop_pid == None :
+                flash('執行動作失敗，請聯絡管理者。','alert-danger')
+                return redirect(url_for('show_entries'))
+
             g.db.execute('update prints set status="1" where print_id="'+search_pid[0]+'"')
             g.db.commit()
             app.logger.error('[Print] Stop Printing <PID:'+search_pid[0]+'>')
             return redirect(url_for('show_entries'))
         elif request.form['submit'] == "領取":
+
+            done_pid=g.db.execute('select print_id from prints where status="2" and print_id="'+request.form['print_id']+'"')
+            done_pid=done_pid.fetchone()
+
+            if done_pid == None :
+                flash('執行動作失敗，請聯絡管理者。','alert-danger')
+                return redirect(url_for('show_entries'))
+
+
             g.db.execute('update prints set status="4" where print_id="'+search_pid[0]+'"')
             g.db.commit()
             app.logger.info('[Print] Receive Printing <PID:'+search_pid[0]+'>')
@@ -214,10 +280,17 @@ def manage_entry():
 def print_done():
     if not session.get('logged_in'):
         abort(401)
+
+    done_pid=g.db.execute('select print_id from prints where status="3"')
+    done_pid=done_pid.fetchone()
+
+    if done_pid == None :
+        flash('執行動作失敗，請聯絡管理者。','alert-danger')
+        return redirect(url_for('show_entries'))
+
     g.db.execute('update prints set status="2" where status="3"')
     g.db.commit()
     return redirect(url_for('show_entries'))
-
 
 
 # 登入/出
@@ -247,9 +320,35 @@ def logout():
     flash('您已經成功登出。')
     return redirect(url_for('show_index'))
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(405)
+def page_not_found(e):
+    return render_template('404.html'), 405
+
+@app.errorhandler(401)
+def page_not_found(e):
+    return render_template('404.html'), 401
+
+
+
+###################################
+## █████████████████████████████ ##
+## █████████████████████████████ ##
+## █████████████████████████████ ##
+## █████████████████████████████ ##
+###################################
+
+### Test if file is allowed ###
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
 ### Creating Gcode ###
-def gcode_creater(print_id):
-    time.sleep(8)
+def gcode_done(print_id):
+    catchu.Gcode_Creater(print_id)
     g.db = connect_db()
     g.db.execute('update prints set status="1" where print_id="'+print_id+'"')
     g.db.commit()
@@ -260,7 +359,7 @@ def gcode_creater(print_id):
 
 ### Compile Gcode ###
 def compilegcode(print_id):
-    with open ('./static/gcodes/' + print_id + '.gcode',"r") as myfile:
+    with open ('./static/gcodes/' + print_id + '.nc',"r") as myfile:
         val=myfile.read().replace('\n', '')
     return val
 
@@ -272,18 +371,21 @@ def sendgcode(print_id,val):
     headers = {'content-type': 'application/json'}
     r = requests.post(url, data=payload, headers=headers)
 
+def image_resize(file_name):
+    gimg = cv2.imread('./static/upload_pic/'+file_name,cv2.IMREAD_GRAYSCALE)
+    width , height = gimg.shape
+    width_max = 500
+    if (width > width_max):
+        height = int(height * float(width_max) / width)
+        width = width_max
+    image = cv2.resize(gimg,(height,width))
+    cv2.imwrite('./static/upload_pic/'+file_name.rsplit('_', 1)[0]+'.jpg',image)
 
-###################################
-## █████████████████████████████ ##
-## █████████████████████████████ ##
-## █████████████████████████████ ##
-## █████████████████████████████ ##
-###################################
+
 
 # app.wsgi_app = ProxyFix(app.wsgi_app)
 
 if __name__ == '__main__':
-
     ###### for demo ######
     os.system('cat ./demo_tab.db > ./pt_tab.db')
     ###### for demo ######
@@ -296,6 +398,7 @@ if __name__ == '__main__':
     app.logger.addHandler(handler)
     ###### for LOG ######
 
+    app.logger.error('[Main] Starting server .....')
     app.run(host='0.0.0.0',port=80)
     # app.run(threaded=True)
 
