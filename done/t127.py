de52bdc03225cce5b61f3f3fd564f2771e6d03dc
joy8303 <4A139049@gmail.com>
2015-02-25 12:23:42 +0800

    dir0 update

--- done/t127.py
+++ done/t127.py
@@ -10,17 +10,37 @@ def direction0(q,gimg,level):
 	color = GetLevel(level)
 	height, width = gimg.shape
 	intr=5
-	zoom=20.0
+	zoom=40.0
 	q_tmp="G17\nM3 S1000\nG0 X0 Y0\n"
+	
 	for x in range(0,height,intr):
-		for y in range(width):
-			if gimg[x][y]<=color: # black
-				if (gimg[x][y-1]>color or y==0): # white || y=0
-					q_tmp += "G0 X" + str(x/zoom) + " Y" + str(y/zoom) + "\n"
-				elif y==(width-1): # if y has gone end.
-					q_tmp += "G1 X" + str(x/zoom) + " Y" + str(y/zoom) + "\n"
-			elif (gimg[x][y-1]<=color and y>0): # black && y > 0
-				q_tmp += "G1 X" + str(x/zoom) + " Y" + str((y-1)/zoom) + "\n"
+		if x%2==0:
+			for y in range(width):
+				if gimg[x][y]<=color: # black
+                               		if (gimg[x][y-1]>color or y==0): # white || y=0
+                                       		q_tmp += "G0 X" + str(x/zoom) + " Y" + str(y/zoom) + "\n"
+                                       		q_tmp += "G0 Z2" + "\n"
+                               		elif y==(width-1): # if y has gone end.
+                                       		q_tmp += "G1 F800 X" + str(x/zoom) + " Y" + str(y/zoom) + "\n"
+                                       		q_tmp += "G0 Z0" + "\n"
+                       		elif (gimg[x][y-1]<=color and y>0): # black && y > 0
+                               		q_tmp += "G1 F800 X" + str(x/zoom) + " Y" + str((y-1)/zoom) + "\n"
+                               		q_tmp += "G0 Z0" + "\n"
+		else:	
+			for y in range((width-1),-1,-1):
+                                if gimg[x][y]<=color: # black
+                                        if (gimg[x][y-1]>color or y==0): # white || y=0
+                                                q_tmp += "G1 X" + str(x/zoom) + " Y" + str(y/zoom) + "\n"
+                                                q_tmp += "G0 Z0" + "\n"
+                                        elif y==(width-1):
+                                                q_tmp += "G0 F800 X" + str(x/zoom) + " Y" + str((y-1)/zoom) + "\n"
+                                                q_tmp += "G0 Z2" + "\n"
+                                elif (gimg[x][y-1]<=color and y>0): # black && y > 0
+                                        q_tmp += "G0 F800 X" + str(x/zoom) + " Y" + str((y-1)/zoom) + "\n"
+                                        q_tmp += "G0 Z2" + "\n"
+
+
+
 	q.send(q_tmp)
 	q.close
 	print("d0 DONE!")
@@ -30,16 +50,19 @@ def direction1(q,gimg,level):
 	height, width = gimg.shape
 	q_tmp=''
 	intr=5
-	zoom=20.0
+	zoom=40.0
 	for y in range(0,width,intr):
 		for x in range(height):
 			if gimg[x][y]<=color: # black
 				if (gimg[x-1][y]>color or x==0): # white || y=0
 					q_tmp += "G0 X" + str(x/zoom) + " Y" + str(y/zoom) + "\n"
+					q_tmp += "G0 Z2" + "\n"
 				elif x==(height-1): # if y has gone end.
-					q_tmp += "G1 X" + str(x/zoom) + " Y" + str(y/zoom) + "\n"
+					q_tmp += "G1 F800 X" + str(x/zoom) + " Y" + str(y/zoom) + "\n"
+					q_tmp += "G0 Z0" + "\n"
 			elif (gimg[x-1][y]<=color and x>0): # black && y > 0
-				q_tmp += "G1 X" + str((x-1)/zoom) + " Y" + str(y/zoom) + "\n"
+				q_tmp += "G1 F800 X" + str((x-1)/zoom) + " Y" + str(y/zoom) + "\n"	
+				q_tmp += "G0 Z0" + "\n"
 	q.send(q_tmp)
 	q.close
 	print("d1 DONE!")
@@ -50,7 +73,7 @@ def direction2(q,gimg,level):
 	height, width = gimg.shape
 	q_tmp=''
 	intr=5
-	zoom=20.0
+	zoom=40.0
 	the_range=[]
 
 	for x in range(height-1,0,-intr):
@@ -69,12 +92,15 @@ def direction2(q,gimg,level):
 			if gimg[tx][ty]<=color: # black
 				if (tx+intr<=height and ty+intr<=width) :
 					if (gimg[tx+intr][ty+intr]<=color or ty==0): # white || y=0
-						q_tmp += "G1 X" + str(tx/zoom) + " Y" + str(ty/zoom) + "\n"
+						q_tmp += "G1 F800 X" + str(tx/zoom) + " Y" + str(ty/zoom) + "\n"
+						q_tmp += "G0 Z2" + "\n"
 					elif ty==width: # if y has gone end.
 						q_tmp += "G0 X" + str(tx/zoom) + " Y" + str(ty/zoom) + "\n"
+						q_tmp += "G0 Z0" + "\n"
 			elif (tx+intr<height and ty+intr<width) :
 				if (gimg[tx+intr][ty+intr]<=color and ty>0): # black && y > 0
 					q_tmp += "G0 X" + str(tx/zoom) + " Y" + str((ty-1)/zoom) + "\n"
+					q_tmp += "G0 Z0" + "\n"
 			tx+=intr
 			ty+=intr
 
@@ -87,7 +113,7 @@ def direction3(q,gimg,level):
 	height, width = gimg.shape
 	q_tmp=''
 	intr=5
-	zoom=20.0
+	zoom=40.0
 	the_range=[]
 	for x in range(0,height,intr):
                 the_range.append([x,0])
@@ -106,13 +132,15 @@ def direction3(q,gimg,level):
                         if gimg[tx][ty]<=color: # black
                                 if (tx+intr<=height and ty-intr>=0) :
 					if (gimg[tx+intr][ty-intr]<=color):
-						q_tmp += "G1 X" + str(tx/zoom) + " Y" + str(ty/zoom) + "\n"
+						q_tmp += "G1 F800 X" + str(tx/zoom) + " Y" + str(ty/zoom) + "\n"
+						q_tmp += "G0 Z2" + "\n"
 					else:
 						q_tmp += "G0 X" + str(tx/zoom) + " Y" + str(ty/zoom) + "\n"
+						q_tmp += "G0 Z0" + "\n"
 			elif (tx+intr<=height and ty-intr>=0):
 				if (gimg[tx+intr][ty-intr]<=color and tx>0 ):
 					q_tmp += "G0 X" + str(tx/zoom) + " Y" + str(ty/zoom) + "\n"
-				
+					q_tmp += "G0 Z0" + "\n"
                         tx-=intr
                         ty+=intr
 	
@@ -144,34 +172,34 @@ if __name__ == '__main__':
 	gimg = cv2.imread('picture.jpg',cv2.IMREAD_GRAYSCALE)
 
 	q0x,q0 = Pipe()
-	q1x,q1 = Pipe()
-	q2x,q2 = Pipe()
-	q3x,q3 = Pipe()
+#	q1x,q1 = Pipe()
+#	q2x,q2 = Pipe()
+#	q3x,q3 = Pipe()
 	p0 = Process(target=direction0,args=(q0,gimg,1,))
-	p1 = Process(target=direction1,args=(q1,gimg,3,))
-	p2 = Process(target=direction2,args=(q2,gimg,5,))
-	p3 = Process(target=direction3,args=(q3,gimg,7,))
+#	p1 = Process(target=direction1,args=(q1,gimg,3,))
+#	p2 = Process(target=direction2,args=(q2,gimg,5,))
+#	p3 = Process(target=direction3,args=(q3,gimg,6,))
 	p0.start()
-	p1.start()
-	p2.start()
-	p3.start()
+#	p1.start()
+#	p2.start()
+#	p3.start()
 
 	q0_r = q0x.recv()
-	q1_r = q1x.recv()
-	q2_r = q2x.recv()
-	q3_r = q3x.recv()
+#	q1_r = q1x.recv()
+#	q2_r = q2x.recv()
+#	q3_r = q3x.recv()
 	print("End of Get the Pipe....")
 
 	p0.join()
 	print("p0 end!")
 
-	p1.join()
+#	p1.join()
 	print("p1 end!")
 
-	p2.join()
+#	p2.join()
 	print("p2 end!")
 
-	p3.join()
+#	p3.join()
 	print("p3 end!")
 
 	print("Make the file.....")
@@ -179,9 +207,9 @@ if __name__ == '__main__':
 	file_id = str(filename) + '.nc'
 	f = open(file_id,'w')
 	f.write(q0_r)
-	f.write(q1_r)
-	f.write(q2_r)
-	f.write(q3_r)
+#	f.write(q1_r)
+#	f.write(q2_r)
+#	f.write(q3_r)
 	f.close()
 
 #cv2.imshow('pic-gray',gimg)
