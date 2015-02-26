2794f6596449b42da1416fe4422776eed3030843
Sean <opjlmi@gmail.com>
2015-02-26 23:48:24 +0800

    merge catchu.py (v.1.1)

--- done/catchu.py
+++ done/catchu.py
@@ -9,21 +9,21 @@ from progressbar import AnimatedMarker, Bar, BouncingBar, Counter, ETA, \
     ProgressBar, ReverseBar, RotatingMarker, \
     SimpleProgress, Timer, AdaptiveETA, AdaptiveTransferSpeed
 
-#direcion top to bottom
-
-def direction0(q,gimg,level):
-    color = GetLevel(level)
-    height, width = gimg.shape
-    intr=5
-    zoom=80.0
+def dirINIT(height,width,zoom):
     q_tmp="G17\rM3 S1000\rG0 X0 Y0\r"
-#frame    
     q_tmp += "G0 Z2" + "\r"
     q_tmp += "G1 X" + str(-(height-1)/zoom) +" Y0" + "\r"
     q_tmp += "G1 Y" + str(-(width-1)/zoom) + "\r"
     q_tmp += "G1 X0 " + "\r"
     q_tmp += "G1 Y0" + "\r"
     q_tmp += "G0 Z0" + "\r"
+    return q_tmp
+
+
+def direction0(q,gimg,level,intr,zoom):
+    color = GetLevel(level)
+    height, width = gimg.shape
+
 
     for x in range(0,height,intr):
         if x%2==0:
@@ -53,12 +53,11 @@ def direction0(q,gimg,level):
     q.send(q_tmp)
     q.close
 
-def direction1(q,gimg,level):
+def direction1(q,gimg,level,intr,zoom):
     color = GetLevel(level)
     height, width = gimg.shape
     q_tmp=''
-    intr=5
-    zoom=80.0
+
     for y in range(0,width,intr):
         if y%2==0:
             for x in range(height):
@@ -87,12 +86,10 @@ def direction1(q,gimg,level):
     q.send(q_tmp)
     q.close
 
-def direction2(q,gimg,level):
+def direction2(q,gimg,level,intr,zoom):
     color = GetLevel(level)
     height, width = gimg.shape
     q_tmp=''
-    intr=5
-    zoom=20.0
     the_range=[]
 
     for x in range(height-1,0,-intr):
@@ -107,12 +104,15 @@ def direction2(q,gimg,level):
             if gimg[tx][ty]<=color: # black
                 if (tx+intr<=height and ty+intr<=width) :
                     if (gimg[tx+intr][ty+intr]<=color or ty==0): # white || y=0
-                        q_tmp += "G1 X" + str(tx/zoom) + " Y" + str(ty/zoom) + "\r"
+                        q_tmp += "G1 F800 X" + str(-tx/zoom) + " Y" + str(-ty/zoom) + "\r"
+                        q_tmp += "G0 Z2" + "\r"
                     elif ty==width: # if y has gone end.
-                        q_tmp += "G0 X" + str(tx/zoom) + " Y" + str(ty/zoom) + "\r"
+                        q_tmp += "G0 X" + str(-tx/zoom) + " Y" + str(-ty/zoom) + "\r"
+                        q_tmp += "G0 Z0" + "\r"
             elif (tx+intr<height and ty+intr<width) :
                 if (gimg[tx+intr][ty+intr]<=color and ty>0): # black && y > 0
-                    q_tmp += "G0 X" + str(tx/zoom) + " Y" + str((ty-1)/zoom) + "\r"
+                    q_tmp += "G0 X" + str(-tx/zoom) + " Y" + str(-(ty-1)/zoom) + "\r"
+                    q_tmp += "G0 Z0" + "\r"
             tx+=intr
             ty+=intr
 
@@ -120,41 +120,37 @@ def direction2(q,gimg,level):
     q.close
 
 
-def direction3(q,gimg,level):
+def direction3(q,gimg,level,intr,zoom):
     color = GetLevel(level)
     height, width = gimg.shape
     q_tmp=''
-    intr=5
-    zoom=20.0
     the_range=[]
+
     for x in range(0,height,intr):
         the_range.append([x,0])
-        print "X %d " %(x)
     for y in range(0,width,intr):
         the_range.append([height-1,y])
-    print(the_range)
     for t_range in the_range:
-        print t_range
         tx=t_range[0]
         ty=t_range[1]
         q_tmp += "G0 X" + str(tx/zoom) + " Y" + str(ty/zoom) + "\r"
-        print "X %d Y %d" %(tx,ty)
         while (tx-intr>=0 and ty+intr<=width):
-            print "X1 %d Y1 %d" %(tx,ty)
             if gimg[tx][ty]<=color: # black
-                    if (tx+intr<=height and ty-intr>=0) :
-                        if (gimg[tx+intr][ty-intr]<=color):
-                            q_tmp += "G1 X" + str(tx/zoom) + " Y" + str(ty/zoom) + "\r"
-                        else:
-                            q_tmp += "G0 X" + str(tx/zoom) + " Y" + str(ty/zoom) + "\r"
+                if (tx+intr<=height and ty-intr>=0) :
+                    if (gimg[tx+intr][ty-intr]<=color):
+                        q_tmp += "G1 F800 X" + str(-tx/zoom) + " Y" + str(-ty/zoom) + "\r"
+                        q_tmp += "G0 Z2" + "\r"
+                    else:
+                        q_tmp += "G0 X" + str(-tx/zoom) + " Y" + str(-ty/zoom) + "\r"
+                        q_tmp += "G0 Z0" + "\r"
             elif (tx+intr<=height and ty-intr>=0):
                 if (gimg[tx+intr][ty-intr]<=color and tx>0 ):
-                    q_tmp += "G0 X" + str(tx/zoom) + " Y" + str(ty/zoom) + "\r"
+                    q_tmp += "G0 X" + str(-tx/zoom) + " Y" + str(-ty/zoom) + "\r"
+                    q_tmp += "G0 Z0" + "\r"
             tx-=intr
             ty+=intr
     q.send(q_tmp)
     q.close
-    print("d3 DONE!")
 
 
 def GetLevel(level):
@@ -175,20 +171,30 @@ def GetLevel(level):
 
 
 def Gcode_Creater(print_id):
-    # bar status
-    pbar = ProgressBar(widgets=[Percentage(), Bar()], maxval=100).start()
+    ###### Setting ######
+    
+    zoom=80.0
+    intr0=5
+    intr1=3
 
-    #pic to gray
+    ###### Setting ######
+
+    pbar = ProgressBar(widgets=[Percentage(), Bar()], maxval=100).start()
     gimg = cv2.imread('./static/upload_pic/'+print_id +'.jpg',cv2.IMREAD_GRAYSCALE)
+    height, width = gimg.shape
+ 
     # parent_conn, child_conn = Pipe()
     q0x,q0 = Pipe()
     q1x,q1 = Pipe()
     q2x,q2 = Pipe()
     q3x,q3 = Pipe()
-    p0 = Process(target=direction0,args=(q0,gimg,1,))
-    p1 = Process(target=direction1,args=(q1,gimg,2,))
-    p2 = Process(target=direction2,args=(q2,gimg,3,))
-    p3 = Process(target=direction3,args=(q3,gimg,5,))
+    
+    p0 = Process(target=direction0,args=(q0,gimg,1,intr0,zoom,))
+    p1 = Process(target=direction1,args=(q1,gimg,2,intr0,zoom,))
+    p2 = Process(target=direction2,args=(q2,gimg,3,intr1,zoom,))
+    p3 = Process(target=direction3,args=(q3,gimg,5,intr1,zoom,))
+
+    init_r = dirINIT(height,width,zoom)
     p0.start()
     p1.start()
     p2.start()
@@ -202,35 +208,18 @@ def Gcode_Creater(print_id):
     q3_r = q3x.recv()
 
     p0.join()
-    pbar += 20
-
     p1.join()
-    pbar += 20
-
     p2.join()
-    pbar += 20
-
     p3.join()
-    pbar += 20
-
 
     f = open('./static/gcodes/'+str(print_id) + '.nc','w')
+    f.write(init_r)
     f.write(q0_r)
     f.write(q1_r)
     f.write(q2_r)
     f.write(q3_r)
     f.close()
-    pbar += 10
+
+    pbar += 90
     time.sleep(0.5)
     pbar.finish()
-
-#cv2.imshow('pic-gray',gimg)
-#cv2.waitKey(0)
-#0-31 black
-#32-63
-#64-95
-#96-127
-#128-159
-#160-191
-#192-223
-#224-255 white
