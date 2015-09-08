3caa65a572590a0fb3cad024fa904c51f2dc87f8
root <root@debian>
2015-09-08 15:57:07 +0800

    creat outline.py

--- done/outline.py
+++ done/outline.py
@@ -1,6 +1,6 @@
 import cv2
-import numpy as np,sys
-
+import numpy as np
+g = cv2.imread('picture.jpg')
 img = cv2.imread('picture.jpg',cv2.IMREAD_GRAYSCALE)
 
 #----------------------------------------------------------------------
@@ -13,15 +13,26 @@ img = cv2.imread('picture.jpg',cv2.IMREAD_GRAYSCALE)
 
 ret,thresh1 = cv2.threshold(img,223,255,cv2.THRESH_BINARY)
 ret,thresh2 = cv2.threshold(img,223,255,cv2.THRESH_BINARY_INV)
+
+image,contours,hierarchy=cv2.findContours(thresh2,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+
+
+print len(contours)
+print contours[7] 
+for x in range(0,len(contours[7])):
+	print contours[7][x][0]
+
+#cv2.drawContours(img,contours,0,(0,0,255),3)
+
 #ret,thresh3 = cv2.threshold(img,127,255,cv2.THRESH_TRUNC)
 #ret,thresh4 = cv2.threshold(img,127,255,cv2.THRESH_TOZERO)
 #ret,thresh5 = cv2.threshold(img,127,255,cv2.THRESH_TOZERO_INV)
 
 
-cv2.imshow("img",img)
-cv2.imshow("1",thresh1)
-cv2.imshow("2",thresh2)
-cv2.imshow("3",thresh3)
-cv2.imshow("4",thresh4)
-cv2.imshow("5",thresh5)
+#cv2.imshow("img",g)
+#cv2.imshow("1",thresh1)
+#cv2.imshow("2",thresh2)
+#cv2.imshow("3",thresh3)
+#cv2.imshow("4",thresh4)
+#cv2.imshow("5",thresh5)
 cv2.waitKey(0)
